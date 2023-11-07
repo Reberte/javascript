27082d13a5b1d9a88189c7f0f8422fcb41e4efb8
Marcos Reberte <133255732+Reberte@users.noreply.github.com>
2023-11-07 10:46:34 -0300

    criando exercicio ex013 Switch
    Criando o exercicio 13 usando a condição switch case

--- CursoJS/aula12/ex013.js
+++ CursoJS/aula12/ex013.js
@@ -12,3 +12,29 @@ var diaSem = agora.getDay()
 6 = sábado
 */
 
+switch (diaSem){
+  case 0:
+    console.log('Domingo')
+    break
+  case 1:
+    console.log('Segunda-Feira')
+    break
+  case 2:
+    console.log('Terça-Feira')
+    break
+  case 3:
+    console.log('Quarta-Feira')
+    break
+  case 4:
+    console.log('Quinta-Feira')
+    break
+  case 5:
+    console.log('Sexta-Feira')
+    break
+  case 6:
+    console.log('Sábado')
+    break
+  default:
+    console.log('[ERRO] Data invalida')
+    break
+}
