43b5142637f2d68254eb18317d558d194e8bd2a7
Marcos Reberte <133255732+Reberte@users.noreply.github.com>
2023-11-09 10:27:05 -0300

    criando exemplo repeticao while e do while

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+while (c <= 6){
+  console.log(`Passo ${c}`)
+  c++
+}
+
+var b = 1
+do {
+  console.log(`Processo ${b}`)
+  b++
+} while (b <= 6)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
